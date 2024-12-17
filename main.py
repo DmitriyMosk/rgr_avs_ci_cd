21ad8437daf483656f07df8e7f12a040da357c16
Dmitriy <36143011+DmitriyMosk@users.noreply.github.com>
2024-12-17 21:04:41 +0700

    Update main.py

--- main.py
+++ main.py
@@ -2,5 +2,6 @@ if __name__ == "__main__":
     a = 5 
     b = 6 
     c = 7
-    a_b_c_sum = sum(a,b,c) 
+    a_b_sum = sum(a,b) 
+    a_b_c_sum = sum(a_b_sum, c)
     print(f"Moskovskikh Dmitry IA-232 {a_b_c_sum}") 
